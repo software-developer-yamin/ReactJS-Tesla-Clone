90032421ad235de719b85842c30677da3f23a204
DeveloperYamin <developer.yamin@gmail.com>
2022-03-01 17:14:07 +0600

    create itemText section and buttonGroup section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,5 +1,54 @@
+import styled from "styled-components";
+
 function Section() {
-  return <div>Section</div>;
+  return (
+    <Wrap>
+      <ItemText>
+        <h1>Model-S</h1>
+        <p>Order Online For Touchless Delivery</p>
+      </ItemText>
+      <ButtonGroup>
+        <LeftButton>Custom Order</LeftButton>
+        <RightButton>Exiting Inventory</RightButton>
+      </ButtonGroup>
+    </Wrap>
+  );
 }
 
 export default Section;
+
+const Wrap = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-image: url("/images/model-s.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  align-items: center;
+`;
+const ItemText = styled.div`
+  padding-top: 16vh;
+  text-align: center;
+`;
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+`;
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+`;
+const RightButton = styled(LeftButton)``;

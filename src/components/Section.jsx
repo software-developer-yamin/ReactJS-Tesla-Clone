e971b7a0475285267993d58ad607082c8523a43b
DeveloperYamin <developer.yamin@gmail.com>
2022-03-01 17:37:19 +0600

    add animation in DownArrow

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -53,12 +53,14 @@ const LeftButton = styled.div`
   text-transform: uppercase;
   font-size: 12px;
   cursor: pointer;
+  margin: 8px;
 `;
 const RightButton = styled(LeftButton)``;
 
 const DownArrow = styled.img`
   margin-top: 20px;
   height: 40px;
+  animation: animateDown infinite 1.5s;
 `;
 
 const Buttons = styled.div``

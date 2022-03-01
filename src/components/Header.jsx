c764820719ae0c08efa688c77e560ed97a5e02de
DeveloperYamin <developer.yamin@gmail.com>
2022-03-01 19:03:45 +0600

    Create and Style the Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <a>
+        <img src="/images/logo.svg" alt="" />
+      </a>
+    </Container>
+  );
 }
 
 export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display:flex;
+  align-items: center;
+  padding: 0 20px;
+`;

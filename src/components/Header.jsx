ebdc90aa36cb322d94d30d8f7a6c70f1525adb5b
DeveloperYamin <developer.yamin@gmail.com>
2022-03-01 19:29:21 +0600

    Create and Style the Main Menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,16 @@ function Header() {
       <a>
         <img src="/images/logo.svg" alt="" />
       </a>
+      <Menu>
+        <a href="#">Model S</a>
+        <a href="#">Model 3</a>
+        <a href="#">Model X</a>
+        <a href="#">Model Y</a>
+      </Menu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+      </RightMenu>
     </Container>
   );
 }
@@ -15,7 +25,32 @@ export default Header;
 const Container = styled.div`
   min-height: 60px;
   position: fixed;
-  display:flex;
+  display: flex;
   align-items: center;
   padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+`;
+const RightMenu = styled.div`
+
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+  }
 `;
